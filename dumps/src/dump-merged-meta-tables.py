3c4e5ff8e7dc850f743767f2fc46d329a3b14d77
Bob Borges <robert.d.borges@gmail.com>
2024-03-06 12:56:53 +0100

    feat: add dumps dir and src

---
+++ dumps/src/dump-merged-meta-tables.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Dump some of metadata related to MPs, Ministers, and Speakers merged on the person_id.
+"""
+from pyriksdagen.metadata import load_Corpus_metadata
+import argparse
+
+
+
+def main():
+    corpus = load_Corpus_metadata()
+    for file_ in ['member_of_parliament', 'minister', 'speaker']:
+        df  = corpus[corpus['source'] == file_]
+
+        # Sort the df to make easier for git
+        sortcols = list(df.columns)
+        print(f"sort by {sortcols}")
+        df = df.sort_values(sortcols)
+        df.to_csv(f"{args.outfolder}/{file_}.csv", index=False)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--outfolder", type=str, default="dumps/dumps")
+    args = parser.parse_args()
+    main(args)
